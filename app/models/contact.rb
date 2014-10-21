c284dadd12119412bcfd46143850c45bc7b75239
ccleung <clemsquared@gmail.com>
2014-10-20 23:08:58 -0400

    allow more than one phone number to be created

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
 	belongs_to :user
 	has_many :phone_numbers
-	accepts_nested_attributes_for :phone_numbers
+	accepts_nested_attributes_for :phone_numbers, :allow_destroy => true
 	validates_presence_of :first_name
 end
