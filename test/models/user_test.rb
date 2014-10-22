660d99600c74487a0b3f3a6f6d9bfe095badbbf8
ccleung <clemsquared@gmail.com>
2014-10-22 02:43:39 -0400

    unit tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   test "New user with just an email" do
+   	 @user = User.new
+   	 @user.email = "admin@example.com"
+     assert_not @user.save
+   end
+
+   test "New user with password too short" do
+   	 @user = User.new
+   	 @user.email = "admin@example.com"
+   	 @user.password = "123"
+   	 @user.password_confirmation = "123"
+     assert_not @user.save
+   end
+
+   test "New user with sufficient password" do
+   	 @user = User.new
+   	 @user.email = "admin@examplessss.com"
+   	 @user.password = "12345678"
+   	 @user.password_confirmation = "12345678"
+     assert @user.save
+   end
+
+   test "New user without confirmation password matching" do
+   	 @user = User.new
+   	 @user.email = "admin@examplessss.com"
+   	 @user.password = "12345678"
+   	 @user.password_confirmation = "12345678a"
+     assert_not @user.save
    end
 end
