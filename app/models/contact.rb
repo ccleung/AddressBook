8b5ee25d3a0fbcfb474a6e8576a1c83c25a5da51
ccleung <clemsquared@gmail.com>
2014-10-21 23:42:24 -0400

    make sure our referenced items get destroyed with us

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
 	belongs_to :user
-	has_many :phone_numbers
-	has_many :addresses
+	has_many :phone_numbers, dependent: :destroy
+	has_many :addresses, dependent: :destroy
 	accepts_nested_attributes_for :phone_numbers, :allow_destroy => true
 	accepts_nested_attributes_for :addresses, :allow_destroy => true
 	validates_presence_of :first_name
